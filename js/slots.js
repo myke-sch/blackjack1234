5ce748f83fc44ea5077aec6a87ce4d26ed437eee
mikas <mika.scheibler@online.de>
2022-12-07 22:37:15 +0100

    ngr

--- js/slots.js
+++ js/slots.js
@@ -14,21 +14,63 @@ const items = [
     "jackpot"
 ];
 
+let spielerGuthaben = 500
+
 window.onload = function() {
     eingabeÜberprüfen()
+    document.getElementById("guthaben").innerHTML = `Guthaben: ${spielerGuthaben}€`
 };
 
+
+function gewinnPruefen(slot1,slot2,slot3) {
+    let einsatz = parseFloat(document.getElementById("einsatz").value)
+    if(einsatz < 0) {
+        einsatz = einsatz * -1
+    }
+    let gewinnNachricht = document.getElementById("jackpot_h1")
+    let spielerGuthabenText = document.getElementById("guthaben")
+    spielerGuthabenText.innerHTML = `Guthaben: ${spielerGuthaben - einsatz}€`
+    spielerGuthaben = (spielerGuthaben -= einsatz).toFixed(2)
+    gewinnNachricht.style.visibility = "visible"
+    if(slot1 === slot2  && slot1  === slot3){
+        switch (slot1){
+            case "🍭":
+                gewinnNachricht.innerHTML = `Herzlichen Glückwunsch | Gewinn: ${einsatz * 1.2}`
+                spielerGuthaben += einsatz * 1.2
+                break;
+            case "💵":
+                gewinnNachricht.innerHTML = `Herzlichen Glückwunsch | Gewinn: ${einsatz * 1.5}`
+                spielerGuthaben += einsatz * 1.5
+                break;
+            case "🦖":
+                gewinnNachricht.innerHTML = `Herzlichen Glückwunsch | Gewinn: ${einsatz * 1.8}`
+                spielerGuthaben += einsatz * 1.5
+                break;
+
+            default:
+                gewinnNachricht.innerHTML = `Herzlichen Glückwunsch | Einsatz zurück: ${einsatz}`
+                spielerGuthaben += einsatz
+                break;
+        }
+    }
+    else {
+        gewinnNachricht.innerHTML = `Leider kein Gewinn `
+    }
+}
+
 function eingabeÜberprüfen() {
+
+    let newSlot1 = items[Math.floor(Math.random() * 12)]
+    let newSlot2 = items[Math.floor(Math.random() * 12)]
+    let newSlot3 = items[Math.floor(Math.random() * 12)]
+    let counter = 0;
     let slot1symbol = document.getElementById("slot1Symbol")
     let slot2symbol = document.getElementById("slot2Symbol")
     let slot3symbol = document.getElementById("slot3Symbol")
     let slotcontaienr = document.getElementsByClassName("flex-child")
-    let newSlot1 = items[Math.floor(Math.random() * 12)]
-    let newSlot2 = items[Math.floor(Math.random() * 12)]
-    let newSlot3 = items[Math.floor(Math.random() * 12)]
     let spinBtn = document.getElementById("spinButton")
     spinBtn.addEventListener("click", () => {
-        for(let i = 0; i < 5; i++){
+            gewinnPruefen(newSlot1, newSlot2, newSlot3);
             slot1symbol.style.transition = "0.2s"
             slot2symbol.style.transition = "0.2s"
             slot3symbol.style.transition = "0.2s"
@@ -42,9 +84,10 @@ function eingabeÜberprüfen() {
                 slot1symbol.style.top = "-110px"
                 slot2symbol.style.top = "-110px"
                 slot3symbol.style.top = "-110px"
-                slot1symbol.innerHTML = items[Math.floor(Math.random() * 12)]
-                slot2symbol.innerHTML = items[Math.floor(Math.random() * 12)]
-                slot3symbol.innerHTML = items[Math.floor(Math.random() * 12)]
+
+                slot1symbol.innerHTML = items[newSlot1]
+                slot2symbol.innerHTML = items[newSlot2]
+                slot3symbol.innerHTML = items[newSlot3]
             }, "200")
             setTimeout(() => {
                 slot1symbol.style.transition = "0.2s"
@@ -57,13 +100,6 @@ function eingabeÜberprüfen() {
                 slot2symbol.innerHTML = items[Math.floor(Math.random() * 12)]
                 slot3symbol.innerHTML = items[Math.floor(Math.random() * 12)]
             }, "500")
-
-        }
-
-
-
-
-
     });
 }
 
